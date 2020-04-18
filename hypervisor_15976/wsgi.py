604b94da735573eed75bde3af303a25496479021
Crowdbotics <team@crowdbotics.com>
2020-04-18 11:11:49 +0000

    Update models

--- hypervisor_15976/wsgi.py
+++ hypervisor_15976/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hypervisor_15976.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hypervisor_15976.settings")
 
 application = get_wsgi_application()
